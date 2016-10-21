9fb223cf9c8e52b241312646858b19b4469f360b
vishal1997 <mailtovishal@tatanagar.com>
2016-10-21 19:07:36 +0530

    using the concept of histogram

--- unique_string.c
+++ unique_string.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int check_string(char s[]);
 int main()
 {
     char str[100];
-    printf("Enter a string:\n");
+    printf("Enter a String:\n");
     scanf("%[^\n]s",str);
-    int i,j;
-    int temp=0;
-    int len=strlen(str);
-    for(i=0; i<=len-1; i++)
-    {
-        for(j=i+1; j<=len; j++)
-        {
-            if(str[i]==str[j])
-                temp=1;
-        }
-    }
-    if(temp==0)
-        printf("True");
+    int result=check_string(str);
+    if(result==-1)
+        printf("Not unique");
     else
-        printf("False");
+        printf("Unique");
     return;
+}
+int check_string(char s[])
+{
+    int len=strlen(s);
+    int array[256]={0};
+    int i,index=0;
+    for(i=0;i<len;i++)
+    {
+        index=(int)s[i]-'\0';
+        if(array[index]==0)
+            ++array[index];
+        else
+            return -1;
+    }
+    return 1;
 }
