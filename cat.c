20e013d08faeea710f50e27ed210954d810fcfb5
vishal1997 <mailtovishal@tatanagar.com>
2017-01-16 17:56:27 +0530

    error cleared

--- cat.c
+++ cat.c
@@ -1,26 +1,38 @@
+
+
 #include <string.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int fish,days;
-    scanf("%d",&fish);
-    scanf("%d",&days);
-    int each_day[days];
-    int i,sum=0;
-    for(i=0;i<days;i++)
-    {
-        scanf("%d",&each_day[i]);
-    }
-    for(i=0;i<days;i++)
-    {
-        if(sum<fish)
-            sum=sum+each_day[i];
-        else
+    char str[1000];
+    scanf("%[^\n]%*c", str);
+    char* cat="cat";
+    int length=strlen(str);
+    int length_cat=strlen(cat);
+    if(length<=0)
         {
-            printf("%d",i-1);
-            return (0);
-        }
+        printf("There is no box\n");
+        return 0;
     }
-    printf("Happy Cat!");
+    char* d=strstr(str,cat);
+    int position=d-str;
+    int i=0;
+    int k=0;
+    while(i<position)
+        {
+        if(str[i]==' ')
+            {
+            k++;
+        }
+        i++;
+    }
+    if((k+1)%10==1)
+    printf("The cat is the %dst item in the box",k+1);
+    else if((k+1)%10==2)
+         printf("The cat is the %dnd item in the box",k+1);
+    else if((k+1)%10==3)
+         printf("The cat is the %drd item in the box",k+1);
+    else
+         printf("The cat is the %dth item in the box",k+1);
 }
