78b51e86d95608ca3824e678a6adff82a91c899c
vishal1997 <mailtovishal@tatanagar.com>
2017-05-17 08:22:59 +0530

    bug removed

--- betweennessCentrality.cpp
+++ betweennessCentrality.cpp
@@ -18,33 +18,38 @@ Graph::Graph(int vertex) {
 }
 
 void Graph::addEdge(int src, int dest) {
-    adj[src].push_back(dest);
-    //adj[dest].push_back(src);
+    adj[src].push_back(dest);                   //create adjacent list
 }
 
 void Graph::centrality() {
-    vector <float> bCentrality(vertex,0);
+    vector <float> bCentrality(vertex,0);       //initialize betweenness centrality with 0 for all vertex
     for(int src=0;src<vertex;++src) {
         stack <int> S;
-        //printf("\n%d",src);
-        list<int> P[vertex];
-        vector<int> sigma(vertex,0);
-        sigma[src]=1;
-        vector <int> dist(vertex,-1);
+        list<int> P[vertex];                    //list of all the predecessors of each vertex
+        vector<int> sigma(vertex,0);            //No. of shortest path
+        sigma[src]=1;                           //No.of shortest path where vertex 'src' lies
+        vector <int> dist(vertex,-1);           //total distance from src to dest
         dist[src]=0;
-        list <int> Q;
-        Q.push_back(src);
-        list <int>::iterator w;
+        list <int> Q;                           //create a Queue
+        Q.push_back(src);                       //enqueue the current node
+        list <int>::iterator w;                 //w is a iterator, to get all the adjacent vertex
         while(!Q.empty()) {
             int v=Q.front();
-            Q.pop_front();
-            S.push(v);
+            Q.pop_front();                      //dequeue a vertex
+            S.push(v);                          //push the same vertex in stack which will be used for calculating the dependencies
+
+            //get all the adjacent vertex
             for(w=adj[v].begin();w!=adj[v].end();w++) {
+                //if the distance is less than zero than enqueue the adjacent vertex
+                //distance of the adjacent from the src = distance of its predecessor + 1
                 if(dist[*w]<0) {
                     Q.push_back(*w);
                     dist[*w]=dist[v]+1;
                 }
-                if(dist[*w]=dist[v]+1) {
+                //if the distance of successor is equal to distance of predecessor +1 than
+                //No. of shortest path where a vertex lies= sum of the no. of shortest path(current) + No. of shortest path of its predecessor
+                //append the predecessor to the list of predecessor for the vertex
+                if(dist[*w]==dist[v]+1) {
                     sigma[*w]=sigma[*w]+sigma[v];
                     P[*w].push_back(v);
                 }
@@ -53,18 +58,17 @@ void Graph::centrality() {
         vector <float> delta(vertex,0.0);
         list<int> :: iterator v;
         while(!S.empty()) {
-            int w=S.top();
-            S.pop();
-            for(v=P[w].begin();v!=P[w].end();v++) {
+            int w=S.top();              //get the vertex in non-increasing order
+            S.pop();                    //pop the vertex
+            //get all the predecessor of the vertex
+            for(v=P[w].begin();v!=P[w].end();v++)
                 delta[*v]=delta[*v]+(float)(sigma[*v]/sigma[w])*(1+delta[w]);
-            }
             if(w!=src)
                 bCentrality[w]=bCentrality[w]+delta[w];
         }
     }
-    for(int i=0;i<vertex;i++) {
+    for(int i=0;i<vertex;i++)
         cout<<endl<<i<<"\t"<<bCentrality[i];
-    }
 }
 
 int main()
